6ac9a8d4dc46c18cecc0a7a0acd93e1ff8827da0
Myles Burt <myles_burt@rocketmail.com>
2021-02-25 23:24:21 +1030

    JS file linked to html and onclick events added to progress user from start button to info card.

---
+++ index.js
@@ -0,0 +1,18 @@
+//Getting required elements
+const startButton = document.querySelector(".startButton button");
+const infoBox = document.querySelector(".infoBox");
+const exitBtn = infoBox.querySelector(".buttons .quit");
+const continueBtn = infoBox.querySelector(".buttons .restart");
+
+//If Start Quiz button is clicked...
+startButton.onclick = () => {
+  infoBox.classList.add("activeInfo"); //Show info box
+};
+
+//If Exit button is clicked...
+exitBtn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); //Hide info box
+};
+
+//Dev check to see if JS File is connected
+console.log("Hello World");
