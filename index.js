21bebaaf1b0d5eda68b16536bbf4c97659ce435f
Myles Burt <myles_burt@rocketmail.com>
2021-02-26 01:07:34 +1030

    Correct and wrong answers are colour coded. Questions array is being generated nicely with js.

--- index.js
+++ index.js
@@ -20,23 +20,36 @@ continueBtn.onclick = () => {
   infoBox.classList.remove("activeInfo"); //Hide info box
   quizBox.classList.add("activeQuiz"); //Show quiz box
   showQuestions(0);
+  queCounter(1);
 };
 
 let queCount = 0;
+let queNumb = 1;
 
 const nextBtn = quizBox.querySelector(".nextBtn");
 
 //If Next button clicked...
 nextBtn.onclick = () => {
-  queCount++;
-  showQuestions(queCount);
+  if (queCount < questions.length - 1) {
+    queCount++;
+    queNumb++;
+    showQuestions(queCount);
+    queCounter(queNumb);
+  } else {
+    console.log("Questions completed.");
+  }
 };
 
 //Getting questions and options from array
 function showQuestions(index) {
   const queText = document.querySelector(".queText");
   const optionList = document.querySelector(".optionList");
-  let queTag = "<span>" + questions[index].question + "</span>";
+  let queTag =
+    "<span>" +
+    questions[index].numb +
+    ". " +
+    questions[index].question +
+    "</span>";
   let optionTag =
     `<div class="option">` +
     questions[index].options[0] +
@@ -52,6 +65,33 @@ function showQuestions(index) {
     `<span></span></div>`;
   queText.innerHTML = queTag;
   optionList.innerHTML = optionTag;
+  const option = optionList.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[queCount].answer;
+  if (userAns == correctAns) {
+    answer.classList.add("correct");
+    console.log("Answer is correct");
+  } else {
+    answer.classList.add("incorrect");
+    console.log("Answer is wrong");
+  }
+}
+
+function queCounter(index) {
+  const bottomQuesCounter = quizBox.querySelector(".totalQue");
+  let totalQuestCountTag =
+    `<span><p>` +
+    index +
+    `</p>of<p>` +
+    questions.length +
+    `</p>questions</span>`;
+  bottomQuesCounter.innerHTML = totalQuestCountTag;
 }
 
 //Dev check to see if JS File is connected
