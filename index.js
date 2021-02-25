6f21dfdc90c286d2914de93117d91ce6afda4373
Myles Burt <myles_burt@rocketmail.com>
2021-02-26 00:07:51 +1030

    cards are incrementing through the questions.

--- index.js
+++ index.js
@@ -19,7 +19,40 @@ exitBtn.onclick = () => {
 continueBtn.onclick = () => {
   infoBox.classList.remove("activeInfo"); //Hide info box
   quizBox.classList.add("activeQuiz"); //Show quiz box
+  showQuestions(0);
 };
+
+let queCount = 0;
+
+const nextBtn = quizBox.querySelector(".nextBtn");
+
+//If Next button clicked...
+nextBtn.onclick = () => {
+  queCount++;
+  showQuestions(queCount);
+};
+
+//Getting questions and options from array
+function showQuestions(index) {
+  const queText = document.querySelector(".queText");
+  const optionList = document.querySelector(".optionList");
+  let queTag = "<span>" + questions[index].question + "</span>";
+  let optionTag =
+    `<div class="option">` +
+    questions[index].options[0] +
+    `<span></span></div>` +
+    `<div class="option">` +
+    questions[index].options[1] +
+    `<span></span></div>` +
+    `<div class="option">` +
+    questions[index].options[2] +
+    `<span></span></div>` +
+    `<div class="option">` +
+    questions[index].options[3] +
+    `<span></span></div>`;
+  queText.innerHTML = queTag;
+  optionList.innerHTML = optionTag;
+}
 
 //Dev check to see if JS File is connected
 console.log("Hello World");
